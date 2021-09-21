b0dc1f438ed8529e32b76673a35bf8d590b39ec3
Daniel <d.c.Poolman@student.tudelft.nl>
2021-09-21 16:23:48 +0200

    Naar pandas

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pandas as pd
+
 
 # data is an array, each row (first index) contains an array of length 2
 # that array has as first index the year + quarter (string) and as second index the GDP (float)
 # data[i][1] -> the GDP of the i-th row
-data = np.loadtxt('data.csv', delimiter=',', skiprows=1, dtype=np.dtype(np.str_, np.float_))
+file = './data.csv'
+data = pd.read_csv(file, delimiter=',', names=['year', 'gdp'], skiprows=1)
 print(data)
-
