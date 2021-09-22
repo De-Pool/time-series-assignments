6c75456dce01655d2f47070b68568a96b77a5f97
Daniel <d.c.Poolman@student.tudelft.nl>
2021-09-22 22:48:35 +0200

    Opdracht 1

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,13 +1,18 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+# import statsmodels.graphics.tsaplots as stats
 
 import helper_functions
 
 # data is a dataframe, first column is year, second column is GDP
 data = pd.read_csv('./data.csv', delimiter=',', names=['year', 'gdp'], skiprows=1)
+data['datetime'] = pd.to_datetime(data['year'])
 
 # Opdracht 1
+plt.plot(data['datetime'], data['gdp'])
+plt.show()
+
 # acfs is an array with [0, acf(1), acf(2), ..., acf(period)]
 acfs = helper_functions.sample_acf(data, period=12)
 plt.bar(np.arange(1, len(acfs)), acfs[1:])
